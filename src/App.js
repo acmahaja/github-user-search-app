8f476823771bb520e728708ee5834a6fe0d9101f
@acmahaja <acmahaja@gmail.com>
2022-06-23 20:08:41 +0200

    Created Navbar file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./css/main.css";
+import "./css/dark.css";
+import "./css/light.css";
 
+import {Navbar} from "./components/navbar"
 
 const App = () => {
-  console.log(process.env.REACT_APP_GITHUB_TOKEN);
-  return (
-    <div className="App">
-      <h1>Github User Search App</h1>
-    </div>
-  )
+  const [user, setUser] = useState("github");
+  const [userData, setUserData] = useState({});
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
 
-}
+  useEffect(() => {
+    setTheme(theme);
+  });
+
+  async function getData() {
+    const { data } = await axios.get(`https://api.github.com/users/${user}`);
+    setUserData(data);
+  }
+
+  const updateTheme = (newTheme) => {
+    setTheme(newTheme);
+    localStorage.setItem("theme", theme);
+    console.log(newTheme);
+  };
+
+  // updateTheme('dark')
+
+  return (
+    <div className={`useBorderBox App ${theme}`}>
+      <Navbar />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/navbar.css"
+
+export const Navbar = ({theme}) => {
+  return <nav>
+    <h1>devfinder</h1>
+    <div id="themeToggle">
+        
+    </div>
+  </nav>;
+};
+
+export default Navbar;
